554937f0cd96b07d17345381386256b685baa2a7
kavinraj <kavinrajcst@gmail.com>
2022-04-30 22:00:37 +0530

    Designed Navigation menu items

---
+++ app/src/main/java/com/flytekart/ui/activity/BaseActivityNew.java
@@ -0,0 +1,17 @@
+package com.flytekart.ui.activity;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class BaseActivityNew extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private View navHeader;
+
+
+}
